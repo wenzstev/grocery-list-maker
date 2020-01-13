1a4e2ce25c7214e803375952f0bc6f1a27584824
wenzstev <wenzelsteve@gmail.com>
2020-01-13 17:44:50 -0500

    First commit

---
+++ GroceryListMaker.py
@@ -0,0 +1,86 @@
+#! python3
+
+# Grocery List Maker
+# Takes in a list of urls and creates a list of grocery ingredients for shopping
+# options to export list and manually add items to it
+
+import requests, lxml, re, sys, pyperclip, shelve, pprint
+from bs4 import BeautifulSoup
+from recipe_parser import RecipeNLP
+
+grocery_list_shelf = shelve.open('grocery_list_data')
+if 'grocery_list' in grocery_list_shelf:  # if this not the first time we've opened the program
+    print("getting grocery list from file")
+    grocery_list = grocery_list_shelf['grocery_list']  # dict containing all items, ordered like "ingredient":"amount"
+else:
+    print("creating new grocery list")
+    grocery_list = {}
+
+
+def parse_recipe(url):
+    # TODO: capture url webpage and open
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'lxml')
+
+    # TODO: scan webpage for ingredients
+    ingredient_lines = soup.find_all("span", itemprop="recipeIngredient")
+
+    nlp = RecipeNLP()
+
+    for line in ingredient_lines:
+        parsed_line = nlp.parse(line.text)
+
+        ingredient = parsed_line.ingredient
+        measurement = parsed_line.measurement
+        amount = parsed_line.amount
+
+        # TODO: add item to list if necessary. if not, add additional reference to item
+        if ingredient in grocery_list:
+            if measurement in grocery_list[ingredient]:  # if we've used the same measurement before
+                grocery_list[ingredient][measurement] += amount
+        else:
+            grocery_list[ingredient] = {measurement: amount}
+
+        # TODO: remove qualifiers from ingredient (to reduce duplicates)
+
+
+    # TODO: prompt to ask for additional ingredients
+
+    # TODO: prompt to ask for manual adding of ingredients
+
+    # TODO: prompt to ask for export
+
+    grocery_list_shelf['grocery_list'] = grocery_list  # save updated list to file
+
+
+def clear_list():
+    if 'grocery_list' in grocery_list_shelf:
+        grocery_list_shelf['grocery_list'] = {}
+        print("cleared the grocery list")
+
+
+def print_list():
+    print(" For all recipes, you will need: ")
+    grocery_string = ""
+    for k, v in grocery_list.items():
+        for measurement, amount in v.items():
+            if measurement is not None:
+                if amount is not 0:
+                    grocery_string += str(amount) + " " + measurement + " "
+
+        grocery_string += k + "\n"
+
+    print(grocery_string)
+
+
+if len(sys.argv) < 2:
+    print("no arguments. assuming url is on clipboard")
+    recipes_to_add = str(pyperclip.paste())
+    print("adding {} ".format(recipes_to_add))
+    parse_recipe(recipes_to_add)
+elif sys.argv[1] == 'clear':
+    clear_list()
+elif sys.argv[1] == 'print':
+    print_list()
+else:
+    recipes_to_add = sys.argv[1:]
---
+++ recipe_parser.py
@@ -0,0 +1,93 @@
+import spacy, pprint, re
+from spacy import displacy
+from fractions import Fraction
+
+
+test_recipe_text = ["1 cup flour, sifted",
+                    "1 teaspoons sea salt",
+                    "1 egg",
+                    "1/2 cup milk",
+                    "1 rounded tablespoon baking powder",
+                    "2 tablespoons olive oil",
+                    "3 cups lightly toasted sesame seeds",
+                    "1 (8 oz) package ground beef"]
+
+recipe_example = {'flour': {'cup': 1},
+                  'salt': {'teaspoon': 1},
+                  'egg': {'whole': 1},
+                  'milk': {'cup': .5},
+                  'baking powder': {'tablespoon': 1}}
+
+
+class RecipeNLP:
+    def __init__(self):
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def parse(self, recipe_line):
+        return ParsedLine(self.nlp, recipe_line)
+
+
+class ParsedLine:
+    def __init__(self, nlp, recipe_line):
+        nlp_ing = nlp(recipe_line)
+        self.measurement_regex = re.compile('(cup|tablespoon|teaspoon|oz|pound|ounce|clove|cube)s?')
+
+        self.ingredient = ""
+        self.measurement = ""
+        self.amount = 0
+
+        print()
+        print(recipe_line)
+
+        for token in nlp_ing:
+            print(token, token.dep_, token.pos_)
+
+            # TODO: determine the amount
+            if self.measurement_regex.search(token.text):
+                print("the measurement is {}".format(token))
+                self.measurement = self.measurement_regex.search(token.text).group(1)
+            # TODO: determine the ingredient
+            elif token.dep_ in ['ROOT', 'nsubj', 'dobj', 'appos']:
+                if token.pos_ in ['NOUN', 'PROPN']:
+                    print("the ingredient is {}".format(token))
+
+                    if token.lower_ in ['salt', 'water']:   # ignore certain things that pretty much everyone has
+                        print('ingredient matches "ignore" list')
+                        continue
+
+
+                    # TODO: check if there are any compound words
+                    print(list(token.children))
+                    print('checking children for compounds')
+                    for child in token.children:
+                        print('checking {}'.format(child))
+                        if not self.measurement_regex.match(child.text):
+                            print('not a measurement')
+                            if child.dep_ in ['compound', 'amod']:
+                                print('found a compound')
+                                self.ingredient += child.text + " "
+
+                    self.ingredient += token.text + " "
+
+                if token.pos_ == 'NUM':  # sometimes the amount is registered as nsubj
+                    self.amount = Fraction(token.text)
+            # TODO: determine the measurement (if any)
+            elif token.pos_ == 'NUM':
+                print("the amount is {}".format(token))
+                self.amount = Fraction(token.text)
+            elif token.pos_ == 'X' and token.dep_ == 'nummod':
+                print("the amount is {}".format(token))
+                self.amount = Fraction(token.text)
+
+
+
+if __name__ == '__main__':
+    grocery_list = {}
+    recipe_nlp = RecipeNLP()
+
+    for line in test_recipe_text:
+        parsed_line = recipe_nlp.parse(line)
+        grocery_list[parsed_line.ingredient] = {parsed_line.measurement: parsed_line.amount}
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(grocery_list)
