fd3b75651f3d61504958789b46aaf2df8122008e
wenzstev <wenzelsteve@gmail.com>
2020-02-02 11:26:53 -0500

    changed training_data_maker to work as a command line script

--- model_scripts/training_data_maker.py
+++ model_scripts/training_data_maker.py
@@ -1,5 +1,8 @@
-import re, pprint, plac, os
+import re, pprint, plac, importlib
 from pathlib import Path
+
+
+
 
 @plac.annotations(
     training_file=("The filename containing the text you wish to annotate", "option", "tf", Path),
@@ -8,21 +11,46 @@ from pathlib import Path
 def main(training_file=None, entity_type=None):
     """Script to more easily annotate spaCy NER training examples"""
 
+    list_to_annotate = []
+    complete_training_data = []
+
     if not training_file:
         training_file = input("Please enter the filename of the data you wish to annotate: ")
-        # TODO: check the extension to determine what sort of file was entered
+
+    if not entity_type:
+        entity_type = input("Please enter the name of the entity you wish to annotate for: ")
 
 
+    # TODO: check the extension to determine what sort of file was entered
+    if training_file.suffix == '.txt': # we expect raw lines of text, no annotations
+        name_of_list = training_file.stem   # save the name for later
         with open(training_file, 'r') as training_file:
-            list_to_annotate = training_file.readline()
+            for line in training_file:
+                list_to_annotate.append(line)
 
-        print(list_to_annotate)
 
-    complete_training_data = []
+    elif training_file.suffix == '.py': # we expect partially annotated text here
+        training_file = importlib.import_module(training_file.stem)
+        print("Lists found in this module: ")
+        print([item for item in dir(training_file) if not item.startswith("__")])
+
+        chosen_list = input("Please type the name of the list you would like to annotate: ")
+        if chosen_list in dir(training_file):
+            list_to_annotate = eval("training_file.{}".format(chosen_list))
+
+        name_of_list = chosen_list
+
+    else:
+        print("Unsupported format entered. Trying to parse.")
+        with open(training_file, 'r') as training_file:
+            for line in training_file:
+                list_to_annotate.append(line)
+
+
     # check if the training set is already formatted properly
-    is_formatted = isinstance(training_file[0], tuple)  # TODO: more rigorous checks for alternate data types
+    is_formatted = isinstance(list_to_annotate[0], tuple)
 
-    # TODO: loop through all lines in the training set
+    # loop through all lines in the training set
     for line in list_to_annotate:
         if is_formatted:
             raw_text = line[0]
@@ -33,7 +61,7 @@ def main(training_file=None, entity_type=None):
         while not found_entities:  # repeat the function until all matches are found
             found_entities = entity_search(raw_text)
 
-        # TODO: if so, record the input as the entity and move to the next example
+        # record the input as the entity and move to the next example
         annotated_entity_list = []
         if not isinstance(found_entities, bool):
             annotated_entity_list = [(entity[0], entity[1], entity_type) for entity in found_entities]
@@ -42,7 +70,21 @@ def main(training_file=None, entity_type=None):
 
         complete_training_data.append((raw_text, {"entities": annotated_entity_list}))
 
-    return complete_training_data
+    # choose the file to save and the name of the file
+    filename_to_save = input("Please enter the name of the .py file you would like to save your annotated data to: ")
+    listname_to_save = input("Please enter your name for the annotated data (or leave blank for automatic name):")
+
+    if not listname_to_save:
+        listname_to_save = "{}_annotated_with_{}".format(name_of_list, entity_type)
+
+    if not filename_to_save.endswith('.py'):
+        filename_to_save += '.py'
+
+    with open(filename_to_save, 'a') as annotated_file:
+        pp = pprint.PrettyPrinter()
+        annotated_file.write(listname_to_save + ' = ')
+        annotated_file.write(pp.pformat(complete_training_data))
+        annotated_file.write('\n\n')
 
 
 # helper function to search the line for inputted entities
